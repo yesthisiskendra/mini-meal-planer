36ecceba9c629f5be6946fce7a05fdfe72acb07f
yesthisiskendra <yesthisiskendra@gmail.com>
2019-09-28 12:58:26 -0700

    Single list dropdown, pre-optimization. AKA Dropdown POC

--- src/BoardDD.js
+++ src/BoardDD.js
@@ -1,52 +1,41 @@
 import React from "react";
-import DD2 from "./DD2.js";
+import List from "./List.js";
 
 class BoardDD extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isShowing: false,
-      selectedProtein: "chicken",
-      lists: [
-        {
-          listTitle: "protein",
-          isShowing: false,
-          choices: ["chicken", "beef", "fish"]
-        },
-        {
-          listTitle: "vegetables",
-          isShowing: false,
-          choices: ["broc", "spinach", "squash"]
-        }
-      ]
-    };
+    this.state = { isShowing: false, protein: "" };
     this.toggleShow = this.toggleShow.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
   }
 
-  toggleShow(listTitle) {
-    // this.setState(state => ({
-    //   [listTitle]: { isShowing: !this.state[listTitle].isShowing }
-    // }));
+  toggleShow() {
+    this.setState(state => ({ isShowing: !this.state.isShowing }));
   }
 
   handleSelect(key, value) {
-    // this.setState({ [key]: value });
+    this.setState({ [key]: value, isShowing: false });
   }
 
   render() {
     return (
       <div>
-        {this.state.lists.map(list => (
-          <DD2
-            toggleShow={this.toggleShow}
-            handleSelect={this.handleSelect}
-            listTitle={list.listTitle}
-            choices={list.choices}
-          />
-        ))}
+        <div>
+          <button onClick={this.toggleShow}>Pick a Protein</button>
+        </div>
+        <div>
+          {this.state.isShowing ? (
+            <List
+              handleSelect={this.handleSelect}
+              listTitle={"protein"}
+              choices={["chicken", "steak", "fish"]}
+            />
+          ) : (
+            ""
+          )}
+        </div>
         <h1>MENU</h1>
-        {this.state.selectedProtein}
+        {this.state.protein}
       </div>
     );
   }
